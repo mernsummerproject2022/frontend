d67d3bd0270713b28b3ce4ab4424f307057ecb8a
Alexandru Safta <alexandrusafta44@gmail.com>
2022-08-01 14:18:45 +0300

    added styling to landing page

--- event-planner/src/Pages/LandingPage.jsx
+++ event-planner/src/Pages/LandingPage.jsx
@@ -5,8 +5,19 @@ const LandingPage = () => {
   return (
     <div className="landing">
       <div className="landing-text">
-        <div className="heading">Hello, this is the landing page</div>
-        <Link to="/create">Create new event</Link>
+        <div className="heading">
+          Have you ever wondered what it would be like to be a click away from
+          being able to gather all your friends to have fun?
+          <p>Join now the best event planning app to find out</p>
+        </div>
+        <div className="landing-buttons">
+          <Link to="/login" className="landing-btn">
+            Login
+          </Link>
+          <Link to="/register" className="landing-btn">
+            Register
+          </Link>
+        </div>
       </div>
     </div>
   );
