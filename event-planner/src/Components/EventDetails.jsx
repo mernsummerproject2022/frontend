af4484fec55dceb0266c3f8b43159082e80153f2
cristian.cirdei01@e-uvt.ro <83004817+cris-alt@users.noreply.github.com>
2022-08-04 16:45:25 +0300

    added requests

--- event-planner/src/Components/EventDetails.jsx
+++ event-planner/src/Components/EventDetails.jsx
@@ -1,53 +1,56 @@
 import React from "react";
 
-const EventDetails = ({
-  title,
-  description,
-  location,
-  time,
-  date,
-  duration,
-  type,
-  maxPlayers,
-  invites,
-  owner,
-}) => {
-  const inviteList = invites;
+const EventDetails = ({ event }) => {
+  const invitesToRender = event.owner
+    ? event.invites
+    : event.invites.filter((invite) => invite.accepted === "accepted");
+  const renderRequests = event.owner && event.requests.length ? true : false;
 
   return (
     <div className="wrapper">
       <div className="eventDetailsContainer">
-        <h1>{title}</h1>
-        <h4>{type}</h4>
-        <h2>{description}</h2>
+        <h1>{event.name}</h1>
+        <h4>{event.eventType}</h4>
+        <h2>{event.description}</h2>
         <h3>
-          Location:<span>{location}</span>
+          Location:<span>{event.location.name}</span>
         </h3>
         <h3>
           When:
-          <span>
-            {time}, {date}
-          </span>{" "}
+          <span>{event.dateTime}</span>{" "}
         </h3>
         <h3>
-          Duration:<span>{duration} minutes</span>
+          Duration:<span>{event.duration} minutes</span>
         </h3>
         <h3>
-          Max Participants:<span>{maxPlayers}</span>
+          Max Participants:<span>{event.maxPlayers}</span>
         </h3>
       </div>
       <div>
         <h2>Invites</h2>
-        {inviteList.map((invite, index) => (
+        {invitesToRender.map((invite, index) => (
           <h4 key={index} className="invite">
             {invite.user}
             <span
-              className={`${invite.status} ${owner ? "owner" : "guest"}`}
+              className={`${invite.accepted} ${
+                event.owner ? "owner" : "guest"
+              }`}
             ></span>
           </h4>
         ))}
 
-        {owner && (
+        {renderRequests && (
+          <div>
+            <h2>Requests</h2>
+            {event.requests.map((request, index) => (
+              <h4 key={index} className="invite">
+                {request.user}
+              </h4>
+            ))}
+          </div>
+        )}
+
+        {event.owner && (
           <div>
             <h2>Invite a new friend</h2>
             <input
@@ -61,7 +64,8 @@ const EventDetails = ({
             </button>
           </div>
         )}
-        {!owner && (
+
+        {!event.owner && (
           <div>
             <h2>Request to join</h2>
             <input
--- event-planner/src/Components/EventList.jsx
+++ event-planner/src/Components/EventList.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import Event from '../Components/Event';
+import React from "react";
+import Event from "../Components/Event";
+const EventList = ({ state, actions }) => {
+  // let events=state.eventReducer.events[0];
+  console.log(state.eventReducer.events);
 
-
-const EventList = ({state,actions}) => {
-    
-    
-    // let events=state.eventReducer.events[0];
-    console.log(state.eventReducer.events);
-    
-
-    return (
-        <div className='events_page'>
-            <h1 className='newest_events'>Newest Events</h1>
-            {state.eventReducer.events.map((event,index)=>{
-                return <Event key={index} title={event.name} location={event.location.name} time={event.dateTime} date={event.dateTime} />
-            }
-            )}
-        </div>
-    )
+  return (
+    <div className="events_page">
+      <h1 className="newest_events">Newest Events</h1>
+      {state.eventReducer.events.map((event, index) => {
+        return (
+          <Event
+            key={index}
+            id={event._id}
+            title={event.name}
+            location={event.location.name}
+            time={event.dateTime}
+            date={event.dateTime}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
-    // return ( 
-    //     JSON.parse(events).map(event => 
-    //         <Event key={`event-${event.id}`} title={event.title} location={event.location} time={event.time} date={event.date}/>
-    //     )
-    // )
+// return (
+//     JSON.parse(events).map(event =>
+//         <Event key={`event-${event.id}`} title={event.title} location={event.location} time={event.time} date={event.date}/>
+//     )
+// )
 //};
 
 export default EventList;
