dc34e2e4b071b5bdc1242cd2662aeaf211b34c0a
cristian.cirdei01@e-uvt.ro <83004817+cris-alt@users.noreply.github.com>
2022-08-08 15:29:04 +0300

    code cleaning

--- event-planner/src/Components/Searchbar.jsx
+++ event-planner/src/Components/Searchbar.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import CheckBox from './CheckBox';
+import React from "react";
+import CheckBox from "../Components/CheckBox";
 
-const Searchbar = ({toggleFilter, filterList, filterBy}) => {
-    return ( 
-        <div className='search'>
-            <label>Event Type:</label>
-            {filterList.map((filter, index) => {
-                    return <CheckBox key={index + (new Date().getTime())} 
-                    label={filter} 
-                    filter={filter}
-                    check={filterBy.includes(filter)} 
-                    toggleFilter={toggleFilter}/> })}
-        </div>
-    )
+const Searchbar = ({ toggleFilter, filterList, filterBy }) => {
+  return (
+    <div className="search">
+      <label>Event Type:</label>
+      {filterList.map((filter, index) => {
+        return (
+          <CheckBox
+            key={index + new Date().getTime()}
+            label={filter}
+            filter={filter}
+            check={filterBy.includes(filter)}
+            toggleFilter={toggleFilter}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Searchbar;
